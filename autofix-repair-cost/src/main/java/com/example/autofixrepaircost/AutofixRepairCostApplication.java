b69f40375db41e611b5f7b3b5538b7e142a1db80
Isidora <isidora.oyanedel@usach.cl>
2024-05-17 17:48:26 -0400

    creando eltercer elemento del diagrmaa

---
+++ autofix-repair-cost/src/main/java/com/example/autofixrepaircost/AutofixRepairCostApplication.java
@@ -0,0 +1,13 @@
+package com.example.autofixrepaircost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutofixRepairCostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutofixRepairCostApplication.class, args);
+	}
+
+}
---
+++ autofix-repair-cost/src/test/java/com/example/autofixrepaircost/AutofixRepairCostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autofixrepaircost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutofixRepairCostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
